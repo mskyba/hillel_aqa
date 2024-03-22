8c1c4fd9076eccb8e9233f451bf001161243e322
skyba <mykola.skyba@smartyads.com>
2024-03-22 18:15:40 +0200

    - solved exceptions HW

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws MyException{
+        int array[] = new int[]{0,1,23};
 
+        try {
+            int number = givingNumberTwo();
+            if (number > 0){
+                System.out.println("'If' with my exception"); // just added for test is it condition reachable
+                throw new MyException("My exception added");
+            }
+        }
+        catch (MyException exception){
+            System.out.println(exception.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            System.out.println(array[3]);
+        }
+        catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println("Array length " + array.length + " --> Set other value!");
+        }
+
+        boolean isItRaining = false;
+        try {
+            if (isItRaining == false) {
+                throw new RuntimeException("It's not raining!", new RuntimeException()); // I'm really creative person, sorry
+            }
+        } catch (RuntimeException exception) {
+            System.out.println("Added exception");
+            exception.getMessage();
+            System.out.println(exception);
+            isItRaining = true;
+        } finally {
+            System.out.println("Finally block added and is it raining now? : "+ isItRaining);
+
+        }
+    }
+
+    public static int givingNumberTwo(){
+        return 2;
     }
 }
---
+++ src/MyException.java
@@ -0,0 +1,5 @@
+public class MyException extends Throwable{
+    public MyException(String error) {
+        super(error);
+    }
+}
